e836299b45b8e3f870f2faad5e4b3f9c480bbdf6
Vladimir Kosickij <vladimir.kosickij@gmail.com>
2026-07-07 12:40:13 +0300

    Initial commit

---
+++ beacon/beaconApp.swift
@@ -0,0 +1,20 @@
+//
+//  beaconApp.swift
+//  beacon
+//
+//  Created by Vladimir Kosickij on 07.07.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct beaconApp: App {
+    var body: some Scene {
+        MenuBarExtra {
+            Text("Hello!")
+        } label: {
+            Image(systemName: "circle.fill")
+        }
+    }
+}
